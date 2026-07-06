36aee9c00118955de2233b385db1f45693b4ffe1
nab07lo <34757327+nab07lo@users.noreply.github.com>
2026-07-06 02:09:50 -0300

    Delete src directory

--- src/pages/PricingPage.tsx
+++
@@ -1,9 +0,0 @@
-import Pricing from '../components/Pricing';
-
-export default function PricingPage() {
-  return (
-    <main className="pt-32">
-      <Pricing />
-    </main>
-  );
-}
--- src/pages/Work.tsx
+++
@@ -1,9 +0,0 @@
-import InfiniteGridPortfolio from '../components/InfiniteGridPortfolio';
-
-export default function Work() {
-  return (
-    <main>
-      <InfiniteGridPortfolio />
-    </main>
-  );
-}
